d9b0ab092d10f78e28a98b335941d859f88ecbb1
repulse-86 <156637470+repulse-86@users.noreply.github.com>
2024-12-27 19:56:25 +0800

    feat: set up notification file for notifying customer regarding booking status

---
+++ app/Notifications/BookingPendingApprovalNotification.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\Notifications;
+
+use App\Models\Bus;
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Notifications\Messages\MailMessage;
+use Illuminate\Notifications\Notification;
+
+class BookingPendingApprovalNotification extends Notification implements ShouldQueue
+{
+    use Queueable;
+
+    /**
+     * Create a new notification instance.
+     */
+    public function __construct(public Bus $bus)
+    {
+        //
+    }
+
+    /**
+     * Get the notification's delivery channels.
+     *
+     * @return array<int, string>
+     */
+    public function via(object $notifiable): array
+    {
+        return ['mail'];
+    }
+
+    /**
+     * Get the mail representation of the notification.
+     */
+    public function toMail(object $notifiable): MailMessage
+    {
+        return (new MailMessage)
+            ->subject('Your Ticket Booking is Pending Approval')
+            ->line('We have received your ticket booking request for '.$this->bus->bus_type.' and it has been successfully processed.')
+            ->line('Currently, your booking is awaiting approval. You will be notified once it is approved.')
+                   //->action('View Booking Details', url('/bookings'))
+            ->line('Thank you for choosing our service! We appreciate your trust in us.');
+    }
+
+    /**
+     * Get the array representation of the notification.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(object $notifiable): array
+    {
+        return [
+            //
+        ];
+    }
+}
