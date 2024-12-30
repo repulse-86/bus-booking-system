3a1848fd5843fd7167e63e82afbb98f9cb0e6696
repulse-86 <156637470+repulse-86@users.noreply.github.com>
2024-12-30 09:29:32 +0800

    feat: create admin home page

---
+++ resources/js/Pages/Admin/Home.vue
@@ -0,0 +1,18 @@
+<script setup>
+import AppLayout from '@/Layouts/AppLayout.vue';
+</script>
+
+<template>
+    <AppLayout title="Bookings">
+        <template #header>
+            <h2 class="font-semibold text-xl text-black dark:text-gray-200 leading-tight">
+                Dashboard
+            </h2>
+        </template>
+        <div class="py-12">
+            <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 grid grid-cols-1 lg:grid-cols-3 gap-6">
+                
+            </div>
+        </div>
+    </AppLayout>
+</template>
