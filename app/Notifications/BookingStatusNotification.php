f6f036cd4c8c22d8a5dd6e4043a5f33f0baf2783
repulse-86 <156637470+repulse-86@users.noreply.github.com>
2024-12-30 11:04:01 +0800

    feat: prepare booking status notification

---
+++ app/Notifications/BookingStatusNotification.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace App\Notifications;
+
+use App\Models\BookedTicket;
+use App\Models\Bus;
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Notifications\Messages\MailMessage;
+use Illuminate\Notifications\Notification;
+
+class BookingStatusNotification extends Notification implements ShouldQueue
+{
+    use Queueable;
+
+    /**
+     * Create a new notification instance.
+     */
+    public function __construct(
+        public BookedTicket $bookedTicket, 
+        public Bus $bus, 
+        public string $status)
+    {
+        //
+    }
+
+    /**
+     * Get the notification's delivery channels.
+     *
+     * @return array<int, string>
+     */
+    public function via(object $notifiable): array
+    {
+        return ['mail'];
+    }
+
+    /**
+     * Get the mail representation of the notification.
+     */
+    public function toMail(object $notifiable): MailMessage
+    {
+        $bookingId = $this->bookedTicket->id;
+        $departureLocation = $this->bus->departure_location;
+        $destinationLocation = $this->bus->destination_location;
+
+        return (new MailMessage)
+                    ->subject('Your Ticket Booking is ' . $this->status)
+                    ->line("Your booking #{$bookingId} for the bus from {$departureLocation} to {$destinationLocation} has been {$this->status}.")
+                    ->line('Thank you for using our application!');
+    }
+
+    /**
+     * Get the array representation of the notification.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(object $notifiable): array
+    {
+        return [
+            //
+        ];
+    }
+}
