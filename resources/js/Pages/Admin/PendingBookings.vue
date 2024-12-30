a7221728d66fe9b4458444a00f724bf357615104
repulse-86 <156637470+repulse-86@users.noreply.github.com>
2024-12-30 09:44:41 +0800

    feat: create pending bookings page for admin

---
+++ resources/js/Pages/Admin/PendingBookings.vue
@@ -0,0 +1,75 @@
+<script setup>
+import { capitalize } from 'vue';
+import { Link } from '@inertiajs/vue3';
+import AppLayout from '@/Layouts/AppLayout.vue';
+import Paginator from '@/Components/Paginator.vue';
+
+defineProps({
+    bookings: {
+        type: Object,
+        required: true,
+    }
+});
+</script>
+
+<template>
+    <AppLayout title="Pending Bookings">
+        <template #header>
+            <h2 class="font-semibold text-xl text-black dark:text-gray-200 leading-tight">
+                Pending Bookings
+            </h2>
+        </template>
+        <div class="py-12">
+            <div class="overflow-x-auto max-w-7xl mx-auto bg-white dark:bg-gray-800 shadow-md rounded-lg">
+                <table class="min-w-full divide-y divide-gray-200 dark:divide-gray-700">
+                    <thead class="bg-gray-50 dark:bg-gray-700">
+                        <tr>
+                            <th class="px-6 py-3 text-left font-black text-gray-600 dark:text-gray-300 uppercase tracking-wider w-1/5">ID</th>
+                            <th class="px-6 py-3 text-left font-black text-gray-600 dark:text-gray-300 uppercase tracking-wider w-1/5">Customer</th>
+                            <th class="px-6 py-3 text-left font-black text-gray-600 dark:text-gray-300 uppercase tracking-wider w-1/5">Type</th>
+                            <th class="px-6 py-3 text-left font-black text-gray-600 dark:text-gray-300 uppercase tracking-wider w-1/5">From</th>
+                            <th class="px-6 py-3 text-left font-black text-gray-600 dark:text-gray-300 uppercase tracking-wider w-1/5">To</th>
+                            <th class="px-6 py-3 text-left font-black text-gray-600 dark:text-gray-300 uppercase tracking-wider w-1/5">Price</th>
+                        </tr>
+                    </thead>
+                    <tbody class="bg-white dark:bg-gray-800 divide-y divide-gray-200 dark:divide-gray-700">
+                        <tr v-for="(booking, index) in bookings.data" :key="booking.index">
+                            <td class="px-6 py-4 text-sm text-gray-900 dark:text-gray-200 break-words">
+                                <Link :href="route('customer.booked-tickets.show', booking.id)">
+                                    {{ booking.id }}
+                                </Link>
+                            </td>
+                            <td class="px-6 py-4 text-sm text-gray-900 dark:text-gray-200 break-words">
+                                <Link :href="route('customer.booked-tickets.show', booking.id)">
+                                    {{ capitalize(booking.customer.name) }}
+                                </Link>
+                            </td>
+                            <td class="px-6 py-4 text-sm text-gray-900 dark:text-gray-200 break-words">
+                                <Link :href="route('customer.booked-tickets.show', booking.id)">
+                                    {{ capitalize(booking.bus.bus_type) }}
+                                </Link>
+                            </td>
+                            <td class="px-6 py-4 text-sm text-gray-900 dark:text-gray-200 break-words">
+                                <Link :href="route('customer.booked-tickets.show', booking.id)">
+                                    {{ capitalize(booking.bus.departure_location) }}
+                                </Link>
+                            </td>
+                            <td class="px-6 py-4 text-sm text-gray-900 dark:text-gray-200 break-words">
+                                <Link :href="route('customer.booked-tickets.show', booking.id)">
+                                    {{ capitalize(booking.bus.destination_location) }}
+                                </Link>
+                            </td>
+                            <td class="px-6 py-4 text-sm text-gray-900 dark:text-gray-200 break-words">
+                                <Link :href="route('customer.booked-tickets.show', booking.id)">
+                                    P{{ booking.bus.price_per_ticket }}
+                                </Link>
+                            </td>
+                        </tr>
+                    </tbody>
+
+                </table>
+                <Paginator :object="bookings"/>
+            </div>
+        </div>
+    </AppLayout>
+</template>
