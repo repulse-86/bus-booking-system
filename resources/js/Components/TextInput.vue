bcca27dd0a3ee52ddccc90e78106bb0e7ebd048e
repulse-86 <156637470+repulse-86@users.noreply.github.com>
2024-12-31 08:51:10 +0800

    feat: add placeholder to TextInput component

--- resources/js/Components/TextInput.vue
+++ resources/js/Components/TextInput.vue
@@ -7,6 +7,11 @@ defineProps({
         type: String,
         default: 'text',
     },
+    placeholder: {
+        type: String,
+        default: null,
+        required: false,
+    }
 });
 
 defineEmits(['update:modelValue']);
@@ -29,5 +34,6 @@ defineExpose({ focus: () => input.value.focus() });
         class="border-gray-300 dark:border-gray-700 dark:bg-gray-900 dark:text-gray-300 focus:border-indigo-500 dark:focus:border-indigo-600 focus:ring-indigo-500 dark:focus:ring-indigo-600 rounded-md shadow-sm"
         :value="modelValue"
         @input="$emit('update:modelValue', $event.target.value)"
+        :placeholder="placeholder"
     >
 </template>
