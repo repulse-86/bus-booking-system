99d6ace05c5bcab0cd1ae708957614a64f88d068
repulse-86 <156637470+repulse-86@users.noreply.github.com>
2025-01-02 19:11:21 +0800

    feat: create component to contain each booking from history

---
+++ resources/js/Components/HistoryContainer.vue
@@ -0,0 +1,29 @@
+<script setup>
+import { formatDate } from '@/helpers';
+
+defineProps({
+	booking: Object,
+	required: true,
+});
+</script>
+
+<template>
+	<div>
+	    <div class="p-8">
+	        <p class="mb-2">{{ formatDate(booking.travel_date) }}</p>
+	        <div class="w-full flex justify-between mb-4">
+	            <div>
+	                <h1 class="text-3xl font-black text-blue-600">{{ booking.bus.departure_location }}</h1>
+	                <p class="text-slate-500">Departure location</p>
+	            </div>
+	            <div>
+	                <h1 class="text-3xl font-black text-blue-600 text-end">{{ booking.bus.destination_location }}</h1>
+	                <p class="text-slate-500">Destination location</p>
+	            </div>
+	        </div>
+	        
+	        <div class="text-2xl font-bold text-zinc-600">P {{ booking.bus.price_per_ticket }}</div>
+	    </div>
+	    <hr class="h-px bg-gray-200 border-0 dark:bg-gray-700">
+	</div>
+</template>
