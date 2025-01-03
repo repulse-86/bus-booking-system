d0de8a4819117fd8ff494f039afedf554bba6991
repulse-86 <156637470+repulse-86@users.noreply.github.com>
2025-01-03 09:50:10 +0800

    feat: create function to format numbers

--- resources/js/helpers.js
+++ resources/js/helpers.js
@@ -2,3 +2,14 @@ export const formatDate = (date) => {
     const options = { year: 'numeric', month: 'long', day: 'numeric' };
     return new Date(date).toLocaleDateString('en-US', options);
 };
+
+export function formatNumber(num) {
+    if (num >= 1e9) {
+        return (num / 1e9).toFixed(1) + "B"; // Billions
+    } else if (num >= 1e6) {
+        return (num / 1e6).toFixed(1) + "M"; // Millions
+    } else if (num >= 1e3) {
+        return (num / 1e3).toFixed(1) + "K"; // Thousands
+    }
+    return num.toString();
+}
