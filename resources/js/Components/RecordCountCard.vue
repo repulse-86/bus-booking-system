632f844778ea969e76d5453035bd6e5654bab126
repulse-86 <156637470+repulse-86@users.noreply.github.com>
2025-01-03 10:22:28 +0800

    feat: create dashboard card for displaying simple label with count

---
+++ resources/js/Components/RecordCountCard.vue
@@ -0,0 +1,23 @@
+<script setup>
+import { formatNumber } from '@/helpers';
+
+defineProps({
+	label: {
+		type: String,
+		required: true,
+	},
+	value: {
+		type: Number,
+		required: true,
+	}
+});
+</script>
+
+<template>
+	<div class="">
+	    <div class="max-w-sm w-full bg-white rounded-lg shadow dark:bg-gray-800 p-4 md:p-6 flex items-center justify-between  mb-6">
+	        <h1 class="text-4xl font-black">{{ label }}</h1>
+	        <p class="text-9xl text-green-600 text-end">{{ formatNumber(value) }}</p>
+	    </div>
+	</div>
+</template>
