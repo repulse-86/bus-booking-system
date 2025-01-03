430ecd380facf97134a54b5b0313e73bcb69d9f8
repulse-86 <156637470+repulse-86@users.noreply.github.com>
2025-01-03 09:53:16 +0800

    feat: create donut chart component

---
+++ resources/js/Components/DonutChart.vue
@@ -0,0 +1,122 @@
+<script setup>
+import ApexCharts from 'apexcharts';
+import { onMounted } from 'vue';
+import { formatNumber } from '@/helpers';
+
+const props = defineProps({
+	chartData: Object,
+	label: String,
+});
+
+onMounted(() => {
+    if (document.getElementById("donut-chart") && typeof ApexCharts !== 'undefined') {
+      const chart = new ApexCharts(document.getElementById("donut-chart"), getChartOptions());
+      chart.render();
+    }
+});
+
+const totalCount = props.chartData.reduce((sum, stat) => sum + stat.total_booked_tickets, 0);
+
+const getChartOptions = () => {
+  return {
+    series: props.chartData.map(stat => stat.total_booked_tickets),
+    colors: [
+        "#1C64F2", "#16BDCA", "#9061F9", "#FACC15", "#F43F5E", "#0EA5E9", 
+        "#8B5CF6", "#10B981", "#F59E0B", "#EF4444", "#3B82F6", "#9333EA", 
+        "#14B8A6", "#D97706", "#6B7280", "#6366F1", "#4ADE80"
+    ],
+    chart: {
+      height: 320,
+      width: "100%",
+      type: "donut",
+    },
+    stroke: {
+      colors: ["transparent"],
+      lineCap: "",
+    },
+    plotOptions: {
+      pie: {
+        donut: {
+          labels: {
+            show: true,
+            name: {
+              show: true,
+              fontFamily: "Inter, sans-serif",
+              offsetY: 20,
+            },
+            total: {
+              showAlways: true,
+              show: true,
+              label: props.label,
+              fontFamily: "Inter, sans-serif",
+              formatter: function (w) {
+                const sum = w.globals.seriesTotals.reduce((a, b) => {
+                  return a + b
+                }, 0)
+                return totalCount
+              },
+            },
+            value: {
+              show: true,
+              fontFamily: "Inter, sans-serif",
+              offsetY: -20,
+              formatter: function (value) {
+                return formatNumber(value)
+              },
+            },
+          },
+          size: "80%",
+        },
+      },
+    },
+    grid: {
+      padding: {
+        top: -2,
+      },
+    },
+    labels: props.chartData.map(stat => stat.bus_type),
+    dataLabels: {
+      enabled: false,
+    },
+    legend: {
+      position: "bottom",
+      fontFamily: "Inter, sans-serif",
+    },
+    yaxis: {
+      labels: {
+        formatter: function (value) {
+          return formatNumber(value)
+        },
+      },
+    },
+    xaxis: {
+      labels: {
+        formatter: function (value) {
+          return formatNumber(value)
+        },
+      },
+      axisTicks: {
+        show: false,
+      },
+      axisBorder: {
+        show: false,
+      },
+    },
+  }
+}
+</script>
+
+<template>
+	<div class="">
+		<div class="max-w-sm w-full bg-white rounded-lg shadow dark:bg-gray-800 p-4 md:p-6">
+			<div class="flex justify-between mb-3">
+				<div class="flex justify-center items-center">
+					<h5 class="text-xl font-bold leading-none text-gray-900 dark:text-white pe-1">{{ label }}</h5>
+				</div>
+			</div>
+
+			<!-- Donut Chart -->
+			<div class="py-6" id="donut-chart"></div>
+		</div>
+	</div>
+</template>
