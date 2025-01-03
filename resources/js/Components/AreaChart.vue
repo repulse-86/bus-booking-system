66dca3b2d907663b3a89c0dc550338fb7ea21795
repulse-86 <156637470+repulse-86@users.noreply.github.com>
2025-01-03 17:18:54 +0800

    refactor: remove percentage

--- resources/js/Components/AreaChart.vue
+++ resources/js/Components/AreaChart.vue
@@ -93,12 +93,6 @@ const options = {
 	                <h5 class="leading-none text-3xl font-bold text-gray-900 dark:text-white pb-2">{{ totalCount }}</h5>
 	                <p class="text-base font-normal text-gray-500 dark:text-gray-400">{{ label }} this week</p>
 	            </div>
-	            <div class="flex items-center px-2.5 py-0.5 text-base font-semibold text-green-500 dark:text-green-500 text-center">
-	                12%
-	                <svg class="w-3 h-3 ms-1" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 14">
-	                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 13V1m0 0L1 5m4-4 4 4"/>
-	                </svg>
-	            </div>
 	        </div>
 	        <div id="area-chart"></div>
 	    </div>
