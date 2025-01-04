ec588c824f8a7b7ac19762283e91f20e979a7580
repulse-86 <156637470+repulse-86@users.noreply.github.com>
2025-01-04 13:23:50 +0800

    refactor: optimize layout of booking form

--- resources/js/Pages/Customer/BookingForm.vue
+++ resources/js/Pages/Customer/BookingForm.vue
@@ -7,6 +7,7 @@ import InputError from '@/Components/InputError.vue';
 import PrimaryButton from '@/Components/PrimaryButton.vue';
 import SecondaryButton from '@/Components/SecondaryButton.vue';
 import TextInput from '@/Components/TextInput.vue';
+import Label from '@/Components/Label.vue';
 
 const { auth } = usePage().props;
 
@@ -91,44 +92,47 @@ const resetForm = () => {
                 <form @submit.prevent="submitForm" enctype="multipart/form-data" class="grid grid-cols-1 lg:grid-cols-2 gap-8 p-6">
                     <!-- Left Column -->
                     <div class="bg-white dark:bg-gray-800 p-6 rounded-lg shadow-md w-full space-y-6">
-                        <h1 class="text-4xl font-semibold mb-6 text-left">Booking Details</h1>
-
-                        <div class="grid grid-cols-1 md:grid-cols-2 gap-6 mb-6">
-                            <div class="space-y-6">
+                        <h1 class="text-4xl font-semibold">Booking Details</h1>
+                        <div class="space-y-4">
+                            <p class="text-lg text-gray-600">Choose your seat and select your preferred travel date to complete the booking.</p>
+                        </div>
+                        <div class="grid grid-cols-1 md:grid-cols-2 gap-6 flex-grow">
+                            <div class="space-y-6 flex flex-col justify-center">
                                 <div>
-                                    <label class="font-bold text-2xl text-gray-700">Departure Location</label>
-                                    <p class="text-xl text-gray-900 italic">{{ bus.departure_location }}</p>
+                                    <Label>Departure Location</Label>
+                                    <p class="text-lg text-gray-800">{{ bus.departure_location }}</p>
                                 </div>
                                 <div>
-                                    <label class="font-bold text-2xl text-gray-700">Destination Location</label>
-                                    <p class="text-xl text-gray-900 italic">{{ bus.destination_location }}</p>
+                                    <Label>Destination Location</Label>
+                                    <p class="text-lg text-gray-800">{{ bus.destination_location }}</p>
                                 </div>
                             </div>
-                            <div class="space-y-6">
+                            <div class="space-y-6 flex flex-col justify-center">
                                 <div>
-                                    <label class="font-bold text-2xl text-gray-700">Available Seats</label>
-                                    <p class="text-xl text-gray-900 italic">{{ bus.available_seats }}</p>
+                                    <Label>Number of Seats</Label>
+                                    <p class="text-lg text-gray-800">{{ bus.available_seats }}</p>
                                 </div>
                                 <div>
-                                    <label class="font-bold text-2xl text-gray-700">Price Per Ticket</label>
-                                    <p class="text-xl text-gray-900 italic">P{{ bus.price_per_ticket }}</p>
+                                    <Label>Price Per Ticket</Label>
+                                    <p class="text-lg text-gray-800">P {{ bus.price_per_ticket }}</p>
                                 </div>
                             </div>
                         </div>
 
-                        <div class="grid grid-cols-1 md:grid-cols-2 gap-6 mb-6">
-                            <div class="">
+                        <div class="mt-auto grid lg:grid-cols-2 gap-6">
+                            <div>
                                 <InputLabel value="Select Seat" class="text-lg font-medium mb-2" />
                                 <SecondaryButton 
                                     type="button" 
                                     @click="openModal"
-                                    class="w-full h-10"
+                                    class="w-full py-3"
                                 >
                                     {{ selectedSeatLabel }}
                                 </SecondaryButton>
                                 <InputError class="mt-2 text-red-500" :message="form.errors.seat" />
                             </div>
-                            <div class="mb-6">
+
+                            <div>
                                 <InputLabel value="Travel Date" class="text-lg font-medium mb-2" />
                                 <TextInput v-model="form.travel_date" type="date" class="w-full" />
                                 <InputError class="mt-2 text-red-500" :message="form.errors.travel_date" />
@@ -136,37 +140,34 @@ const resetForm = () => {
                         </div>
                     </div>
 
-                    <!-- Right Column -->
-                    <div class="">
+                    <div class="space-y-6">
                         <div class="bg-white dark:bg-gray-800 p-6 rounded-lg shadow-md w-full space-y-6">
-                            <h1 class="text-4xl font-semibold text-left">Payment Information</h1>
-
+                            <h1 class="text-4xl font-semibold">Payment Information</h1>
                             <div class="space-y-4">
-                                <p class="text-lg text-red-600">Please make your payment to this GCash number and upload your receipt below.</p>
-                                <p class="text-xl font-medium text-gray-900 mb-4">
+                                <p class="text-lg text-red-600">Choose your seat and select your preferred travel date to complete the booking.</p>
+                            </div>
+                            <div class="space-y-6">
+                                <p class="text-xl font-medium text-gray-900">
                                     <strong>GCash Number:</strong> <span class="font-bold text-blue-600">0917-123-4567</span>
                                 </p>
-                            </div>
 
-                            <div class="mb-6">
-                                <InputLabel value="Upload Payment Image" class="text-lg font-medium mb-2" />
-                                <TextInput 
-                                    type="file"
-                                    class="mt-1 w-full py-2 px-4 border rounded-lg"
-                                    @change="handleFileChange"
-                                    id="file" 
-                                />
-                                <InputError class="mt-2 text-red-500" :message="form.errors.payment_image" />
+                                <div class="">
+                                    <InputLabel value="Upload Payment Image" class="text-lg font-medium mb-2" />
+                                    <TextInput 
+                                        type="file"
+                                        class="mt-1 w-full py-2 px-4 border rounded-lg"
+                                        @change="handleFileChange"
+                                        id="file" 
+                                    />
+                                    <InputError class="mt-2 text-red-500" :message="form.errors.payment_image" />
+                                </div>
                             </div>
-
-                            <div class="mt-6">
-                                <PrimaryButton class="w-full" :class="{ 'opacity-25': form.processing }" :disabled="form.processing">
-                                    Submit
-                                </PrimaryButton>
-                            </div>
+                            
+                        <PrimaryButton class="py-3 flex justify-center items-center" :class="{ 'opacity-25': form.processing }" :disabled="form.processing">Submit</PrimaryButton>
                         </div>
                     </div>
                 </form>
+
             </div>
         </div>
 
