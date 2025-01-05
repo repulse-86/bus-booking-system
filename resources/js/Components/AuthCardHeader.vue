538f04abdd3b5934d7c929ff012c476f45a08946
repulse-86 <156637470+repulse-86@users.noreply.github.com>
2025-01-05 16:10:55 +0800

    feat: create a component containing title and subtitle for authentication card

---
+++ resources/js/Components/AuthCardHeader.vue
@@ -0,0 +1,13 @@
+<script setup>
+defineProps({
+	title: String,
+	subTitle: String,
+})
+</script>
+
+<template>
+	<div class="mb-6">
+	    <h1 class="text-3xl text-gray-800 font-bold">{{ title }}</h1>
+	    <p class="text-lg text-gray-500">{{ subTitle }}</p>
+	</div>
+</template>
