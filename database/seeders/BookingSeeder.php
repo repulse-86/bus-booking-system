046e4b0a0174a6db1e605e86903f501775b2415e
repulse-86 <156637470+repulse-86@users.noreply.github.com>
2025-02-02 20:15:00 +0800

    fix: booking seeder assigning booking with non-existent bus id

--- database/seeders/BookingSeeder.php
+++ database/seeders/BookingSeeder.php
@@ -20,7 +20,7 @@ class BookingSeeder extends Seeder
         foreach (range(1, 50) as $index) {
             Booking::create([
                 'customer_id' => 2,
-                'bus_id' => $faker->numberBetween(1, 17),
+                'bus_id' => $faker->numberBetween(1, 15),
                 'seat' => $faker->numberBetween(1, 50),
                 'travel_date' => $faker->dateTimeBetween('now', '+1 year')->format('Y-m-d'),
                 'payment_image' => 'none',
