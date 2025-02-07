0a76b4f97ebd47d86a4d82782b879f4a54c1bfd8
repulse-86 <156637470+repulse-86@users.noreply.github.com>
2025-02-07 15:39:00 +0800

    feat: cache data for showing buses client side

--- resources/js/Layouts/AppLayout.vue
+++ resources/js/Layouts/AppLayout.vue
@@ -81,7 +81,7 @@ onMounted(() => {
                         <div class="flex">
                             <!-- Navigation Links -->
                             <div v-if="$page.props.auth.user.type === 'customer'" class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
-                                <NavLink v-for="link in customerLinks" :key="link.label" :href="route(link.href)" :active="route().current(link.href)">
+                                <NavLink v-for="link in customerLinks" :key="link.label" :href="route(link.href)" :active="route().current(link.href)" prefetch cache-for="1m">
                                     {{ link.label }}
                                 </NavLink>
                             </div>
@@ -245,7 +245,7 @@ onMounted(() => {
                 <div :class="{'block': showingNavigationDropdown, 'hidden': ! showingNavigationDropdown}" class="sm:hidden">
                     <div class="pt-2 pb-3 space-y-1">
                         <div v-if="$page.props.auth.user.type === 'customer'">
-                            <ResponsiveNavLink v-for="link in customerLinks" :key="link.label" :href="route(link.href)" :active="route().current(link.href)">
+                            <ResponsiveNavLink v-for="link in customerLinks" :key="link.label" :href="route(link.href)" :active="route().current(link.href)" prefetch cache-for="1m">
                                 {{ link.label }}
                             </ResponsiveNavLink>
                         </div>
